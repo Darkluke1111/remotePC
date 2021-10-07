342c7965d9339660a3d1e3beaa4521ae2a29f2a5
Lukas <lukas.gromer@uni-ulm.de>
2021-10-07 15:54:32 +0200

    Added lua cleint

---
+++ ccclient.lua
@@ -0,0 +1,33 @@
+local connectionURL = "ws://demos.kaazing.com/echo"
+local ws, err = http.websocket(connectionURL)
+if not ws then
+  return printError(err)
+end
+
+ws.send("Hello world!")
+
+parallel.waitForAny(receive,send)
+
+function receive()
+    while true do
+        local _, url, response, isBinary = os.pullEvent("websocket_message")
+
+        -- We need this if statement to check that we received the message from
+        -- the correct websocket. After all, you can have many websockets connected to
+        -- different URLs.
+        if url == connectionURL then
+            print(response)
+        end  
+    end
+end
+
+function send()
+    while true do
+        local _, username, message = os.pullEvent("chat")
+        local json = textutils.serialize({
+            username = username,
+            message = message
+        })
+        ws.send(json)
+    end
+end
